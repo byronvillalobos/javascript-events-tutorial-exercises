c42716a7b6bd5872718789f6f10d7c6dd8e45f23
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-19 04:02:49 +0000

    my profile

--- exercises/05-the-load-event/index.js
+++ exercises/05-the-load-event/index.js
@@ -1,1 +1,7 @@
 // your function goes here
+
+document.getElementById("start").addEventListener("load", myFunction);
+
+function myFunction() {
+	document.getElementById("start").innerHTML = "loading finished.";
+}
