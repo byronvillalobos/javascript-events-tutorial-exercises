fd0a9493bcffc6c90bad7a4cbefc3f1d08f64aeb
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-19 02:24:46 +0000

    delivered

--- exercises/02-on-click-Hello-World/index.js
+++ exercises/02-on-click-Hello-World/index.js
@@ -1,5 +1,13 @@
 //Declare your function here
 
-window.myClickFunction = function myClickFunction() {
-	alert("Your first function!");
+// window.myClickFunction = function myClickFunction() {
+// 	alert("Your first function!");
+// };
+
+var listenerfunction = function myClickFunction() {
+	alert("First click");
+	document.getElementById("Hello").removeEventListener("click", listenerfunction);
+};
+var listenerfunction2 = function inicialize() {
+	document.getElementById("Hello").addEventListener("click", listenerfunction);
 };
